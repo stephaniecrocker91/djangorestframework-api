43fe75aadec0bb0cc7816805a8b0241d8e487f64
Stephie <stephaniecrocker91@gmail.com>
2022-09-13 21:14:07 +0000

    Debug post serializer, typo in class name

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from .models import Posts
+from posts.models import Post
 
 
-class PostsSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -28,7 +28,7 @@ class PostsSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     class Meta:
-        model = Posts
+        model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
