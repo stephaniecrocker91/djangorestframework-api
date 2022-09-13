77eeeb9b5881bd7b4874960a339de04123300788
Stephie <stephaniecrocker91@gmail.com>
2022-09-13 20:53:27 +0000

    add image_filter chouces field to posts model, validate input of uploaded images

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,6 +8,21 @@ class PostsSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                'Image size too large! Must be under 2MB!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height too large! Must be under 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width too large! Must be under than 4096px!'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -17,5 +32,5 @@ class PostsSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image'
+            'title', 'content', 'image', 'image_filter',
         ]
