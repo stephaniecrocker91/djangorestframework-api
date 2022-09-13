4c54b2192a3d6aba93017a2fb5f0e0c1f674d813
Stephie <stephaniecrocker91@gmail.com>
2022-09-13 20:22:19 +0000

    create posts app, write posts model and serializer

---
+++ posts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Posts
+
+
+class PostsSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Posts
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image'
+        ]
