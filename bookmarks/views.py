d010509b160db0812ff0cff55f89a9cedf19f9fd
Stephie <stephaniecrocker91@gmail.com>
2022-09-15 20:36:25 +0000

    Write generic view for bookamrk list and  detail. and write bookmark api

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from bookmarks.models import Bookmark
+from bookmarks.serializers import BookmarkSerializer
 
-# Create your views here.
+
+class BookmarkList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = BookmarkSerializer
+    queryset = Bookmark.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class BookmarkDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = BookmarkSerializer
+    queryset = Bookmark.objects.all()
