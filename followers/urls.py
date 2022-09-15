3a9319ee41aa33215ff271d03b2cd62d99a3db8a
Stephie <stephaniecrocker91@gmail.com>
2022-09-15 20:06:52 +0000

    Refactor post and profile view code using generic views

--- followers/urls.py
+++ followers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from followers import views
 
 urlpatterns = [
-    path('followers/', views.LikeList.as_view()),
-    path('followers/<int:pk>/', views.LikeDetail.as_view()),
+    path('followers/', views.FollowerList.as_view()),
+    path('followers/<int:pk>/', views.FollowerDetail.as_view()),
 ]
