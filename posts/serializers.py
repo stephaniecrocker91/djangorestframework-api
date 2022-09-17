4c5e5ad452994b34e8c496634a80a5a30ed39166
Stephie <stephaniecrocker91@gmail.com>
2022-09-17 16:42:32 +0000

    write extra data to post and profile serializer: likeid and followerid

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,8 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -27,10 +30,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            liked = Like.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return liked.id if liked else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
+            'like_id',
         ]
