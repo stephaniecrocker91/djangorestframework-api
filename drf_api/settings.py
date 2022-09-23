7feabbf08e69b103186c0d43a1397ad0af3a6457
Stephie <stephaniecrocker91@gmail.com>
2022-09-23 12:50:40 +0000

    Add pagination to api, format date and time, add created_at and updated_at fields to comments

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -30,7 +30,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 8,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
 
 REST_USE_JWT = True
