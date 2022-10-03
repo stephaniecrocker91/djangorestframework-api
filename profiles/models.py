24e584ec7ed6441b9d948fec4bc07197d68ffb62
Stephie <stephaniecrocker91@gmail.com>
2022-10-03 10:11:33 +0000

    Update default profile image to black and white

--- profiles/models.py
+++ profiles/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_rexhwh'
+        upload_to='images/', default='../avatar_profile_mkm0cf'
     )
 
     class Meta:
